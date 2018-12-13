d7345992fa676f93d46eb74c8467559df0bd19dd
IPICGI\IPI <steven.jolivald@ipilyon.net>
2018-12-13 09:27:01 +0100

    premier commit

---
+++ src/main/java/com/fitman/javafitman/JavafitmanApplication.java
@@ -0,0 +1,14 @@
+package com.fitman.javafitman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafitmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafitmanApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/fitman/javafitman/JavafitmanApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fitman.javafitman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavafitmanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
